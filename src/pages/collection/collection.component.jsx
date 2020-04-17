84edb01cc076f09a45d0b7a38937880bcc866ef0
Yury Hrytsuk <jerzy.gricuk@yurys-air.attlocal.net>
2020-04-17 14:47:30 -0400

    changing flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,9 +5,15 @@ import './collection.style.scss';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 const CollectionPage = ({ collection }) => {
-   console.log(collection)
-   return (<div className ="colelction-page">
-      <h2>Colelction page</h2>
+   const { title, items } = collection;
+   return (
+   <div className ="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+         {
+            items.map(item => <CollectionItem key={item.id} item={item}/>)
+         }
+      </div>
    </div>)
 };
 
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,33 +4,33 @@ const INITIAL_STATE = {
          title: 'accessories',
          imageUrl:'https://images.unsplash.com/photo-1576053139778-7e32f2ae3cfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
          id: 1,
-         linkUrl: 'accessories'
+         linkUrl: 'shop/accessories'
       },
          {
          title: 'sneakers',
          imageUrl: 'https://images.unsplash.com/photo-1555195798-e42b6048838e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=987&q=80', 
          id: 2,
-         linkUrl: ''
+         linkUrl: 'shop/sneakers'
       },
          {
          title: 'jackets',
          imageUrl: 'https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
          id: 3,
-         linkUrl: ''
+         linkUrl: 'shop/jackets'
       },
          {
          title: 'womens',
          imageUrl: 'https://images.unsplash.com/photo-1568306415419-3265104f6a41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
          id: 4,
          size: 'large',
-         linkUrl: ''
+         linkUrl: 'shop/womens'
       },
          {
          title: 'mens',
          imageUrl: 'https://images.unsplash.com/photo-1551649478-7513a363db79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
          id: 5,
          size: 'large',
-         linkUrl: ''
+         linkUrl: 'shop/mens'
       }
    ]
 };
