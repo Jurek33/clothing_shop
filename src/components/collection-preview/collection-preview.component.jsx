ff10d2d90a90e8a5b2fe3aed677a8be27b22ae01
Yury Hrytsuk <jerzy.gricuk@yurys-air.attlocal.net>
2020-04-23 09:45:13 -0400

    added saga to root reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './collection-preview.style.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({title, items, history, match}) => (
 	<div className="collection-preview">
-		<h1 className="title">
+		<h1 className="title" onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)} >
 		{title.toUpperCase()}
 		</h1>
 		<div className="preview">
@@ -19,4 +20,4 @@ const CollectionPreview = ({title, items}) => (
 	</div>
 )
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import createSagaMiddleWare from 'redux-saga';
+import rootSaga from './root-saga'
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleWare()
+
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV === 'development') {
    middlewares.push(logger);
@@ -12,4 +15,8 @@ if(process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+sagaMiddleware.run(rootSaga);
+
 export const persistor = persistStore(store);
+
+export default { store, persistStore }
