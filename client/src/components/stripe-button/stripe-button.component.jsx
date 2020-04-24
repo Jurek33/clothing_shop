8b04ee2b5372258dd218f741f980229d002f9c7f
Yury Hrytsuk <jerzy.gricuk@yurys-air.attlocal.net>
2020-04-24 13:39:17 -0400

    moved frontend code to client folder, added express server to handle stripe

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+   const stripePrice = price * 100;
+   const publishableKey = 'pk_test_DLcFSwMlT3HQDqzq6bM8v2qE00VKE3ctQe';
+   const onToken = token => {
+      axios({
+         url: 'payment',
+         method: 'post',
+         data: {
+            amount: stripePrice,
+            token
+         }
+      }).then(response => {
+         alert('Payment Successful')
+      }).catch(error => {
+         console.log('Payment error ', JSON.parse(error));
+         alert('There was an issue with your payment. Please make sure to use provided credit cart info.')
+      })
+   }
+   return (
+      <StripeCheckout 
+         label="Pay Now"
+         name="Clothing Shop LTD"
+         billingAddress
+         shippingAddress
+         description={`Your total is ${price} $`}
+         amount={stripePrice}
+         panelLabel="Pay Now"
+         token={onToken}
+         stripeKey={publishableKey}
+      />
+   );
+};
+
+export default StripeCheckoutButton;
